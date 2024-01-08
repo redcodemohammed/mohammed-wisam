523d504dc6aa74179d36d8eda8653c6bd73c7503
redcodemohammed <redcodemohammed@gmail.com>
2024-01-08 09:54:07 +0300

    Refactor app.vue structure

--- app.vue
+++ app.vue
@@ -1,13 +1,17 @@
 <template>
   <Html class="select-none font-outfit overflow-x-hidden dark">
-    <Head>
-      <title>Mohammed Wisam</title>
-    </Head>
-    <UContainer>
-      <HeroSection />
-      <AboutMe />
-      <ServicesSection />
-      <ProjectsSection />
-    </UContainer>
+
+  <Head>
+    <title>Mohammed Wisam</title>
+  </Head>
+  <UContainer>
+    <HeroSection />
+    <AboutMe />
+    <ServicesSection />
+    <ProjectsSection />
+  </UContainer>
+
+  <ByMo />
+
   </Html>
 </template>
---
+++ components/by-mo.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+const timesPressed = ref(0)
+const increment = () => timesPressed.value++
+
+onMounted(() => {
+  document.addEventListener('keydown', {
+    // make the code listen to the keydown event that type 7780 in order
+    handleEvent: (event: KeyboardEvent) => {
+      if (timesPressed.value === 0 && event.code === 'Digit7') {
+        increment()
+      }
+
+      if (timesPressed.value === 1 && event.code === 'Digit7') {
+        increment()
+      }
+
+      if (timesPressed.value === 2 && event.code === 'Digit8') {
+        increment()
+      }
+
+      if (timesPressed.value === 3 && event.code === 'Digit0') {
+        increment()
+      }
+
+      if (timesPressed.value === 4) {
+        alert('This app was developed by Mohammed Wisam <3 aka @redcodemohammed')
+        timesPressed.value = 0
+      }
+
+      if (event.code !== 'Digit7' && event.code !== 'Digit8' && event.code !== 'Digit0') {
+        timesPressed.value = 0
+      }
+    }
+  })
+})
+</script>
